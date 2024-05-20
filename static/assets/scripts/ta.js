010ffcf801fd2c93b1944ea4c7d9c237e456fc76
KnowledgeWanderer <riderfreezenova@gmail.com>
2024-05-20 15:40:50 +0300

    removed a game and added fs for iframe

--- static/assets/scripts/ta.js
+++ static/assets/scripts/ta.js
@@ -13,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const newTab = document.createElement("li")
     const tabTitle = document.createElement("span")
     const newIframe = document.createElement("iframe")
+    newIframe.allowFullscreen = true;
 
     tabTitle.textContent = `New Tab ${tabCounter}`
     tabTitle.className = "tab-title"
